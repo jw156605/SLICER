f6aaa9a79aea58de649a7c38971f00dbfa69e73c
Joshua Welch <jwelch@cs.unc.edu>
2017-08-16 17:20:40 -0400

    Added documentation for package data

---
+++ R/traj-data.R
@@ -0,0 +1,8 @@
+#' @name traj
+#' @title Simulated trajectory
+#' @description This is a simulated branching trajectory.
+#' @docType data
+#' @usage traj
+#' @format A matrix with samples down rows and genes across columns
+#' @source Simulated data
+#' @author Joshua Welch, 2017-08-16
