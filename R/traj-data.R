c557a52bcb07bce718232d95c960ca753736b605
Joshua Welch <jwelch@broadinstitute.org>
2017-08-17 08:31:20 -0400

    Added dataset documentation

--- R/traj-data.R
+++
@@ -1,8 +0,0 @@
-#' @name traj
-#' @title Simulated trajectory
-#' @description This is a simulated branching trajectory.
-#' @docType data
-#' @usage traj
-#' @format A matrix with samples down rows and genes across columns
-#' @source Simulated data
-#' @author Joshua Welch, 2017-08-16
